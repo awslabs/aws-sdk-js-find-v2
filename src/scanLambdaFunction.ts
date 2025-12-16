085174ad7e52092cbc9c58b44abe9a3de59fbf26
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2025-12-16 10:11:50 -0800

    chore: skip no-unused-vars rule on package.json failure

--- src/scanLambdaFunction.ts
+++ src/scanLambdaFunction.ts
@@ -39,7 +39,7 @@ export const scanLambdaFunction = async (client: Lambda, functionName: string) =
           console.log(`${JS_SDK_V2_MARKER.Y} ${functionName}`);
           return;
         }
-      } catch (error) {
+      } catch (error) { // oxlint-disable-line no-unused-vars
         // Parsing failure for package.json which is rare, continue.
       }
     }
