a98a32709b26d656cf0464badca9c43f117506b4
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2025-12-16 14:04:59 -0800

    feat: use node-stream-zip for unzipping

--- src/utils/getLambdaFunctionContents.ts
+++ src/utils/getLambdaFunctionContents.ts
@@ -1,4 +1,4 @@
-import unzipper from "unzipper";
+import StreamZip from "node-stream-zip";
 const PACKAGE_JSON_FILENAME = "package.json";
 
 export type LambdaFunctionContents = {
@@ -24,33 +24,40 @@ export type LambdaFunctionContents = {
 export const getLambdaFunctionContents = async (
   zipPath: string,
 ): Promise<LambdaFunctionContents> => {
-  const directory = await unzipper.Open.file(zipPath);
+  const zip = new StreamZip.async({ file: zipPath });
 
   const packageJsonContents = [];
-  for (const file of directory.files) {
+  const zipEntries = await zip.entries();
+  for (const zipEntry of Object.values(zipEntries)) {
     // Skip 'node_modules' directory, as it's not the customer source code.
-    if (file.path.includes("node_modules/")) continue;
+    if (zipEntry.name.includes("node_modules/")) continue;
 
     // Skip anything which is not `package.json`
-    if (!file.path.endsWith(PACKAGE_JSON_FILENAME)) continue;
+    if (!zipEntry.name.endsWith(PACKAGE_JSON_FILENAME)) continue;
 
-    const packageJsonContent = await file.buffer();
+    const packageJsonContent = await zip.entryData(zipEntry.name);
     packageJsonContents.push(packageJsonContent.toString());
   }
 
   if (packageJsonContents.length !== 0) {
+    await zip.close();
     return { packageJsonContents };
   }
 
   let indexFile;
   for (const path of ["index.js", "index.mjs", "index.cjs"]) {
-    indexFile = directory.files.find((f) => f.path === path && f.type === "File");
-    if (indexFile) break;
+    try {
+      indexFile = await zip.entryData(path);
+      break;
+    } catch {
+      // File doesn't exist, try next
+    }
   }
 
+  await zip.close();
+
   if (indexFile) {
-    const bundleContent = await indexFile.buffer();
-    return { bundleContent: bundleContent.toString() };
+    return { bundleContent: indexFile.toString() };
   }
 
   return {};
