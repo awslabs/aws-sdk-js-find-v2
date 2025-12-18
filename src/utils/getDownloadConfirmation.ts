657a9948a77ce78bbad2bbceb5ae3670a75ba20d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2025-12-18 15:26:13 -0800

    chore: share maximum disk space used by lambda command

--- src/utils/getDownloadConfirmation.ts
+++ src/utils/getDownloadConfirmation.ts
@@ -5,7 +5,7 @@ import { getHumanReadableBytes } from "./getHumanReadableBytes.ts";
  * Prompts user for confirmation before downloading Lambda function code
  *
  * @param functionCount - Number of Lambda functions to be processed
- * @param totalCodeSize - Total size of all function code in bytes
+ * @param codeSizeToDownload - Total size of all function code in bytes
  * @returns Promise that resolves to boolean indicating user's choice
  * @description
  * - Creates interactive readline interface for user input
@@ -14,7 +14,8 @@ import { getHumanReadableBytes } from "./getHumanReadableBytes.ts";
  */
 export const getDownloadConfirmation = async (
   functionCount: number,
-  totalCodeSize: number,
+  codeSizeToDownload: number,
+  codeSizeToSaveOnDisk: number,
 ): Promise<boolean> => {
   const rl = createInterface({
     input: process.stdin,
@@ -22,9 +23,10 @@ export const getDownloadConfirmation = async (
   });
 
   const answer = await rl.question(
-    `This script will process ${functionCount} Lambda Node.js functions,` +
-      `\nand download ${getHumanReadableBytes(totalCodeSize)} of compressed archives over the network.` +
-      `\nDo you want to continue? (y/N): `,
+    `This command will process ${functionCount} Lambda Node.js functions, and` +
+      `\ndownload ${getHumanReadableBytes(codeSizeToDownload)} of compressed archives over the network.` +
+      `\nIt'll store maximum of ${getHumanReadableBytes(codeSizeToSaveOnDisk)} on disk at any point.` +
+      `\n\nDo you want to continue? (y/N): `,
   );
   rl.close();
 
