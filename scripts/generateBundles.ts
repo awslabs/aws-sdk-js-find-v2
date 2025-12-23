7cc577f409aafa441c67ac1081a6e4dd513993d2
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2025-12-23 15:04:18 -0800

    chore: run bundling serially

--- scripts/generateBundles.ts
+++ scripts/generateBundles.ts
@@ -1,7 +1,5 @@
-import pLimit from "p-limit";
 import { exec } from "node:child_process";
 import { readdir } from "node:fs/promises";
-import { cpus } from "node:os";
 import { join } from "node:path";
 import { promisify } from "node:util";
 
@@ -13,46 +11,24 @@ const execAsync = promisify(exec);
 const configDir = join(import.meta.dirname, "configs");
 const jsFiles = (await readdir(configDir)).filter((file) => file.endsWith(".js"));
 
-const promises = [];
 for (const fileName of jsFiles) {
   const bundlerName = fileName.substring(0, fileName.indexOf("."));
   const filePath = join(configDir, fileName);
 
   if (bundlerName === "esbuild") {
     // esbuild doesn't support config.
-    promises.push(async () => {
-      try {
-        console.log(`[${bundlerName}] Running with code '${fileName}'`);
-        await execAsync(`node ${filePath}`);
-      } catch (error) {
-        throw new Error(`[${bundlerName}] failed`, { cause: error });
-      }
-    });
+    console.log(`[${bundlerName}] Running with code '${fileName}'`);
+    await execAsync(`node ${filePath}`);
     continue;
   }
 
-  promises.push(async () => {
-    try {
-      console.log(`[${bundlerName}] Running with config '${fileName}'`);
-      await execAsync(`npx ${bundlerName} -c ${filePath}`);
-    } catch (error) {
-      throw new Error(`[${bundlerName}] failed`, { cause: error });
-    }
-  });
+  console.log(`[${bundlerName}] Running with config '${fileName}'`);
+  await execAsync(`npx ${bundlerName} -c ${filePath}`);
 }
 
 // Parcel uses configuration from package.json
 for (const version of Object.values(Version)) {
-  promises.push(async () => {
-    const bundlerName = "parcel";
-    try {
-      console.log(`[${bundlerName}] Running for '${version}'`);
-      execAsync(`npx parcel build ${join(getFixturesDir(), version)} --no-cache`);
-    } catch (error) {
-      throw new Error(`[${bundlerName}] failed`, { cause: error });
-    }
-  });
+  const bundlerName = "parcel";
+  console.log(`[${bundlerName}] Running for '${version}'`);
+  await execAsync(`npx parcel build ${join(getFixturesDir(), version)} --no-cache`);
 }
-
-const limit = pLimit(cpus().length || 1);
-await Promise.all(promises.map((promise) => limit(promise)));
