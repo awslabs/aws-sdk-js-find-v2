8eff59f7b2681ed612d0ad10b4108ce52a35c1c0
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2025-12-23 10:01:56 -0800

    chore(ci): run bundle generation in parallel

--- scripts/generateBundles.ts
+++ scripts/generateBundles.ts
@@ -1,5 +1,7 @@
+import pLimit from "p-limit";
 import { exec } from "node:child_process";
 import { readdir } from "node:fs/promises";
+import { cpus } from "node:os";
 import { join } from "node:path";
 import { promisify } from "node:util";
 
@@ -8,17 +10,25 @@ const execAsync = promisify(exec);
 const configDir = join(import.meta.dirname, "configs");
 const jsFiles = (await readdir(configDir)).filter((file) => file.endsWith(".js"));
 
+const promises = [];
 for (const fileName of jsFiles) {
   const bundlerName = fileName.substring(0, fileName.indexOf("."));
   const filePath = join(configDir, fileName);
 
   if (bundlerName === "esbuild") {
     // esbuild doesn't support config.
-    console.log(`Running '${bundlerName}' with code '${fileName}'`);
-    await execAsync(`node ${filePath}`);
+    promises.push(async () => {
+      console.log(`Running '${bundlerName}' with code '${fileName}'`);
+      await execAsync(`node ${filePath}`);
+    });
     continue;
   }
 
-  console.log(`Running '${bundlerName}' with config '${fileName}'`);
-  await execAsync(`npx ${bundlerName} -c ${filePath}`);
+  promises.push(async () => {
+    console.log(`Running '${bundlerName}' with config '${fileName}'`);
+    await execAsync(`npx ${bundlerName} -c ${filePath}`);
+  });
 }
+
+const limit = pLimit(cpus().length || 1);
+await Promise.all(promises.map((promise) => limit(promise)));
