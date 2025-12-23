7da85975bf0fb96a43a87efbccf89f74132bb54a
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-12-23 10:39:09 -0800

    chore(ci): run bundle generation in parallel (#40)

--- scripts/generateBundles.ts
+++ scripts/generateBundles.ts
@@ -1,5 +1,7 @@
+import pLimit from "p-limit";
 import { exec } from "node:child_process";
 import { readdir } from "node:fs/promises";
+import { cpus } from "node:os";
 import { join } from "node:path";
 import { promisify } from "node:util";
 
@@ -8,17 +10,34 @@ const execAsync = promisify(exec);
 const configDir = join(import.meta.dirname, "configs");
 const jsFiles = (await readdir(configDir)).filter((file) => file.endsWith(".js"));
 
+const promises = [];
 for (const fileName of jsFiles) {
   const bundlerName = fileName.substring(0, fileName.indexOf("."));
   const filePath = join(configDir, fileName);
 
   if (bundlerName === "esbuild") {
     // esbuild doesn't support config.
-    console.log(`Running '${bundlerName}' with code '${fileName}'`);
-    await execAsync(`node ${filePath}`);
+    promises.push(async () => {
+      try {
+        console.log(`[${bundlerName}] Running with code '${fileName}'`);
+        await execAsync(`node ${filePath}`);
+      } catch (error) {
+        throw new Error(`[${bundlerName}] failed`, { cause: error });
+      }
+    });
     continue;
   }
 
-  console.log(`Running '${bundlerName}' with config '${fileName}'`);
-  await execAsync(`npx ${bundlerName} -c ${filePath}`);
+  promises.push(async () => {
+    try {
+      console.log(`[${bundlerName}] Running with config '${fileName}'`);
+      await execAsync(`npx ${bundlerName} -c ${filePath}`);
+    } catch (error) {
+      throw new Error(`[${bundlerName}] failed`, { cause: error });
+    }
+  });
 }
+
+const limit = pLimit(cpus().length || 1);
+// Promises are reversed, since slower bundlers (webpack, rollup) are added after faster ones (esbuild, rolldown).
+await Promise.all(promises.reverse().map((promise) => limit(promise)));
