0537fb4bc79c61d656668674771b4af65883ca4f
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2025-12-22 21:19:09 -0800

    chore: move rolldown CLI to configs

---
+++ scripts/configs/rolldown.v2.esm.config.js
@@ -0,0 +1,16 @@
+import { join } from "node:path";
+import { defineConfig } from "rolldown";
+import { Extension, Version } from "./utils/constants.js";
+import { getFixturesDir } from "./utils/getFixturesDir.js";
+import { getInputPath } from "./utils/getInputPath.js";
+import { getOutputFilename } from "./utils/getOutputFilename.js";
+
+export default defineConfig({
+  input: getInputPath(Version.v2),
+  output: {
+    file: join(getFixturesDir(), getOutputFilename("rolldown", Version.v2, Extension.mjs)),
+    format: "esm",
+    minify: true,
+  },
+});
+
