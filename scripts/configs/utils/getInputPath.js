d8cf3d7f37e2000cbf2af6aa3275b4672f8e5f55
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2025-12-23 14:40:08 -0800

    Revert "chore: move index.js file to lib folder"
    This reverts commit 4ee850733e539c034a2b62f6c7920fa089a63bbe.

--- scripts/configs/utils/getInputPath.js
+++ scripts/configs/utils/getInputPath.js
@@ -1,4 +1,4 @@
 import { join } from "node:path";
 import { getFixturesDir } from "./getFixturesDir.js";
 
-export const getInputPath = (version) => join(getFixturesDir(), version, "lib", "index.js");
+export const getInputPath = (version) => join(getFixturesDir(), version, "index.js");
