948ab1cc30cedc8b0bdd42a9133bd188e3b0bc3a
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2025-12-23 21:27:48 -0800

    chore: re-create bundles

--- src/utils/__fixtures__/parcel.v3.cjs
+++ src/utils/__fixtures__/parcel.v3.cjs
@@ -1,13 +1,1 @@
-var $fHheo$awssdkclientsts = require("@aws-sdk/client-sts");
-
-
-function $parcel$export(e, n, v, s) {
-  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
-}
-
-$parcel$export(module.exports, "handler", () => $341871660ad46082$export$c3c52e219617878);
-
-const $341871660ad46082$var$client = new (0, $fHheo$awssdkclientsts.STSClient)();
-const $341871660ad46082$export$c3c52e219617878 = async ()=>$341871660ad46082$var$client.send(new (0, $fHheo$awssdkclientsts.GetCallerIdentityCommand)());
-
-
+var $kCDBf$awssdkclientsts=require("@aws-sdk/client-sts");function $parcel$export(e,t,d,$){Object.defineProperty(e,t,{get:d,set:$,enumerable:!0,configurable:!0})}$parcel$export(module.exports,"handler",()=>$005f572dafede4df$export$c3c52e219617878);const $005f572dafede4df$var$client=new(0,$kCDBf$awssdkclientsts.STSClient),$005f572dafede4df$export$c3c52e219617878=async()=>$005f572dafede4df$var$client.send(new(0,$kCDBf$awssdkclientsts.GetCallerIdentityCommand));
--- src/utils/__fixtures__/parcel.v3.mjs
+++ src/utils/__fixtures__/parcel.v3.mjs
@@ -1,8 +1,1 @@
-import {STSClient as $89kMf$STSClient, GetCallerIdentityCommand as $89kMf$GetCallerIdentityCommand} from "@aws-sdk/client-sts";
-
-
-const $a1824f2350e24476$var$client = new (0, $89kMf$STSClient)();
-const $a1824f2350e24476$export$c3c52e219617878 = async ()=>$a1824f2350e24476$var$client.send(new (0, $89kMf$GetCallerIdentityCommand)());
-
-
-export {$a1824f2350e24476$export$c3c52e219617878 as handler};
+import{STSClient as e,GetCallerIdentityCommand as a}from"@aws-sdk/client-sts";let t=new e,n=async()=>t.send(new a);export{n as handler};
