8ac604871922e815b85a093909ad00ab12bf3c18
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2025-12-24 08:03:43 -0800

    chore: add rspack.config.js

---
+++ scripts/configs/rspack.config.js
@@ -0,0 +1,37 @@
+import rspack from "@rspack/core";
+
+import { ModuleSystem, Version } from "./utils/constants.js";
+import { getFixturesDir } from "./utils/getFixturesDir.js";
+import { getInputPath } from "./utils/getInputPath.js";
+import { getOutputFilename } from "./utils/getOutputFilename.js";
+
+const LibraryType = {
+  [ModuleSystem.cjs]: "commonjs2",
+  [ModuleSystem.esm]: "module",
+};
+
+const createConfig = (version, moduleSystem) => ({
+  target: "node",
+  mode: "production",
+  devtool: false,
+  optimization: {
+    minimizer: [new rspack.SwcJsMinimizerRspackPlugin({ extractComments: false })],
+  },
+  plugins: [new rspack.optimize.LimitChunkCountPlugin({ maxChunks: 1 })],
+  experiments: { outputModule: true },
+  entry: getInputPath(version),
+  output: {
+    path: getFixturesDir(),
+    filename: getOutputFilename("rspack", version, moduleSystem),
+    library: { type: LibraryType[moduleSystem] },
+  },
+});
+
+const configs = [];
+for (const version of Object.values(Version)) {
+  for (const moduleSystem of Object.values(ModuleSystem)) {
+    configs.push(createConfig(version, moduleSystem));
+  }
+}
+
+export default configs;
