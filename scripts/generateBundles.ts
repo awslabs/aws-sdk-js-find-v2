36df5a7d612cacf3a91ab205e3fa1ddbc2f2cd48
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2025-12-23 21:15:29 -0800

    chore: run parcel from generateBundles

--- scripts/generateBundles.ts
+++ scripts/generateBundles.ts
@@ -3,12 +3,14 @@ import { readdir } from "node:fs/promises";
 import { join } from "node:path";
 import { promisify } from "node:util";
 
+import { getFixturesDir } from "./configs/utils/getFixturesDir.js";
+import { Version } from "./configs/utils/constants.js";
+
 const execAsync = promisify(exec);
 
 const configDir = join(import.meta.dirname, "configs");
 const jsFiles = (await readdir(configDir)).filter((file) => file.endsWith(".js"));
 
-const promises = [];
 for (const fileName of jsFiles) {
   const bundlerName = fileName.substring(0, fileName.indexOf("."));
   const filePath = join(configDir, fileName);
@@ -16,20 +18,17 @@ for (const fileName of jsFiles) {
   if (bundlerName === "esbuild") {
     // esbuild doesn't support config.
     console.log(`[${bundlerName}] Running with code '${fileName}'`);
-    promises.push(
-      execAsync(`node ${filePath}`).catch((error) => {
-        throw new Error(`[${bundlerName}] failed`, { cause: error });
-      }),
-    );
+    await execAsync(`node ${filePath}`);
     continue;
   }
 
   console.log(`[${bundlerName}] Running with config '${fileName}'`);
-  promises.push(
-    execAsync(`npx ${bundlerName} -c ${filePath}`).catch((error) => {
-      throw new Error(`[${bundlerName}] failed`, { cause: error });
-    }),
-  );
+  await execAsync(`npx ${bundlerName} -c ${filePath}`);
 }
 
-await Promise.all(promises);
+// Parcel uses configuration from package.json
+for (const version of Object.values(Version)) {
+  const bundlerName = "parcel";
+  console.log(`[${bundlerName}] Running for '${version}'`);
+  await execAsync(`npx parcel build ${join(getFixturesDir(), version)}`);
+}
