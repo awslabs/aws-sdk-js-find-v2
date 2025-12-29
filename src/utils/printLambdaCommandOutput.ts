a40e3ce394d027631730a70854adddf34d856bed
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2025-12-29 15:11:19 -0800

    chore: print table output using console.table

--- src/utils/printLambdaCommandOutput.ts
+++ src/utils/printLambdaCommandOutput.ts
@@ -15,8 +15,33 @@ export const printLambdaCommandOutput = (
   output: LambdaFunctionScanOutput[],
   outputType: LambdaCommandOutputType,
 ) => {
+  // Output as JSON
   if (outputType === LambdaCommandOutputType.json) {
     console.log(JSON.stringify(output, null, 2));
     return;
   }
+
+  // Output as table
+  const tableOutput: Record<string, { Region: string; ContainsAwsSdkJsV2: string }> = {};
+  for (const scanOutput of output) {
+    let notes =
+      scanOutput.ContainsAwsSdkJsV2 === null
+        ? "N/A."
+        : scanOutput.ContainsAwsSdkJsV2
+          ? "Yes."
+          : "No.";
+
+    if (scanOutput.AwsSdkJsV2Error !== undefined) {
+      notes += ` ${scanOutput.AwsSdkJsV2Error}`;
+    }
+    if (scanOutput.AwsSdkJsV2Location !== undefined) {
+      notes += ` ${scanOutput.AwsSdkJsV2Location}`;
+    }
+
+    tableOutput[scanOutput.FunctionName] = {
+      Region: scanOutput.Region,
+      ContainsAwsSdkJsV2: notes,
+    };
+  }
+  console.table(tableOutput);
 };
