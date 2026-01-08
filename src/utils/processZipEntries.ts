d4aed4981730e76a23b14de9c3e03157c007f178
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2026-01-07 20:08:12 -0800

    fix: close zip in case of error in processor

--- src/utils/processZipEntries.ts
+++ src/utils/processZipEntries.ts
@@ -12,16 +12,14 @@ export const processZipEntries = async (
 ) => {
   const zip = new StreamZip.async({ file: zipPath });
 
-  let zipEntries: Record<string, StreamZip.ZipEntry> = {};
+  // Continue with empty object, if zip entries can't be read.
+  const zipEntries = await zip.entries().catch(() => ({}));
+
   try {
-    zipEntries = await zip.entries();
-  } catch {
-    // Continue with empty object, if zip entries can't be read.
+    for (const entry of Object.values(zipEntries)) {
+      await processor(entry, () => zip.entryData(entry.name));
+    }
+  } finally {
+    await zip.close();
   }
-
-  for (const entry of Object.values(zipEntries)) {
-    await processor(entry, () => zip.entryData(entry.name));
-  }
-
-  await zip.close();
 };
