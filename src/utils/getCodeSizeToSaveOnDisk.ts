ffa0117671421089a8b19da43b89a41a142b089b
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-01-08 00:31:42 -0800

    fix: use spread operator to create shallow copy of functions before sorting

--- src/utils/getCodeSizeToSaveOnDisk.ts
+++ src/utils/getCodeSizeToSaveOnDisk.ts
@@ -5,7 +5,7 @@ const getFunctionTotalSize = (fn: FunctionConfiguration) =>
   (fn.CodeSize || 0) + (fn.Layers?.reduce((acc, l) => acc + (l.CodeSize || 0), 0) || 0);
 
 export const getCodeSizeToSaveOnDisk = (functions: FunctionConfiguration[], num: number) => {
-  const largestFunctions = functions
+  const largestFunctions = [...functions]
     .sort((a, b) => getFunctionTotalSize(b) - getFunctionTotalSize(a))
     .slice(0, num);
   const functionSize = largestFunctions.reduce((acc, fn) => acc + (fn.CodeSize || 0), 0);
